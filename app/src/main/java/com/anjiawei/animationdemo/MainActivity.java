c26ca79cc18c360b37eb6f73aeef14488159dd94
anjiawei <anjiawei@anjiaweideMacBook-Pro.local>
2020-05-24 16:12:02 +0800

    增加第三行数据跟随手指滑动显示/隐藏动画

--- app/src/main/java/com/anjiawei/animationdemo/MainActivity.java
+++ app/src/main/java/com/anjiawei/animationdemo/MainActivity.java
@@ -1,30 +1,30 @@
 package com.anjiawei.animationdemo;
 
 
-
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.anjiawei.animationdemo.view.BlurringView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final long ANIMATION_DURATION = 300L;
     private ImageView mBlurredView;
     private static int[] bgResArr = {R.drawable.bg_1, R.drawable.bg_2, R.drawable.bg_3, R.drawable.bg_4, R.drawable.sample_map_background};
     private int mBgResIndex = 0;
     private BlurringView mBlurringView;
     private ImageView mDragBar;
     private RelativeLayout mPanelLayout;
-    private int mDefaultHeight;
-    private int mMaxHeight;
-    private int mMinHeight;
+    private LinearLayout mDataThirdLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         //mBlurringView.setOverlayColor(Color.TRANSPARENT);
         mDragBar = findViewById(R.id.drag_bar);
         mPanelLayout = findViewById(R.id.blurring_view_layout);
+        mDataThirdLine = findViewById(R.id.data_third_line);
     }
 
     @Override
@@ -62,11 +63,20 @@ public class MainActivity extends AppCompatActivity {
             private static final float MIN_HEIGHT_RATIO = 0.2F;
             private static final float MAX_HEIGHT_RATIO = 0.8F;
             private static final float DEFAULT_HEIGHT_RATIO = 0.5F;
+            private int mDefaultHeight;
+            private int mMaxHeight;
+            private int mMinHeight;
+            private int mBlurredViewHeight;
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                int height;
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         mLastY = (int) event.getRawY();
+                        mBlurredViewHeight = mBlurredView.getHeight();
+                        mDefaultHeight = (int) (mBlurredViewHeight * DEFAULT_HEIGHT_RATIO);
+                        mMaxHeight = (int) (mBlurredViewHeight * MAX_HEIGHT_RATIO);
+                        mMinHeight = (int) (mBlurredViewHeight * MIN_HEIGHT_RATIO);
                         break;
                     case MotionEvent.ACTION_MOVE:
                         int offsetY = (int) (mLastY - event.getRawY());
@@ -76,32 +86,51 @@ public class MainActivity extends AppCompatActivity {
                             params.height = mBlurredView.getHeight() - ((int) event.getRawY()) + mDragBar.getHeight();
                             mPanelLayout.setLayoutParams(params);
                         }
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        int height = mPanelLayout.getHeight();
-                        int blurredViewHeight = mBlurredView.getHeight();
-                        int defaultHeight = (int) (blurredViewHeight * DEFAULT_HEIGHT_RATIO);
-                        int maxHeight = (int) (blurredViewHeight * MAX_HEIGHT_RATIO);
-                        int minHeight = (int) (blurredViewHeight * MIN_HEIGHT_RATIO);
+                        height = mPanelLayout.getHeight();
                         if (mIsGoingUp) {
-                            if (height >= 0 && height < minHeight) {
-                                performAnimation(height, minHeight);
-                            } else if (height >= minHeight && height < defaultHeight) {
-                                performAnimation(height, defaultHeight);
-                            } else if (height >= defaultHeight && height < maxHeight) {
-                                performAnimation(height, maxHeight);
-                            } else if (height >= maxHeight && height <= blurredViewHeight) {
-                                performAnimation(height, maxHeight);
+                            if (height >= mDefaultHeight && height <= mMaxHeight) {
+                                if (mDataThirdLine.getVisibility() != View.VISIBLE) {
+                                    mDataThirdLine.setVisibility(View.VISIBLE);
+                                }
+                                float alpha = ((float)(height - mDefaultHeight) / (float) (mMaxHeight - mDefaultHeight));
+                                mDataThirdLine.setAlpha(alpha);
+                            } else if (height >= mMinHeight && height < mDefaultHeight) {
+                                mDataThirdLine.setVisibility(View.INVISIBLE);
                             }
                         } else {
-                            if (height >= 0 && height < minHeight) {
-                                performAnimation(height, minHeight);
-                            } else if (height >= minHeight && height < defaultHeight) {
-                                performAnimation(height, minHeight);
-                            } else if (height >= defaultHeight && height <= maxHeight) {
-                                performAnimation(height, defaultHeight);
-                            } else if (height >= maxHeight && height <= blurredViewHeight) {
-                                performAnimation(height, maxHeight);
+                            if (height >= mDefaultHeight && height <= mMaxHeight) {
+                                float alpha = ((float)(height - mDefaultHeight) / (float) (mMaxHeight - mDefaultHeight));
+                                mDataThirdLine.setAlpha(alpha);
+                            } else if (height >= mMinHeight && height < mDefaultHeight) {
+                                mDataThirdLine.setVisibility(View.INVISIBLE);
+                            }
+                        }
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        height = mPanelLayout.getHeight();
+                        if (mIsGoingUp) {
+                            if (height >= 0 && height < mMinHeight) {
+                                performAnimation(height, mMinHeight);
+                            } else if (height >= mMinHeight && height < mDefaultHeight) {
+                                performAnimation(height, mDefaultHeight);
+                            } else if (height >= mDefaultHeight && height < mMaxHeight) {
+                                performAnimation(height, mMaxHeight);
+                                float alpha = ((float) (height - mDefaultHeight) / (float) (mMaxHeight - mDefaultHeight));
+                                changeAlpha(alpha, 1F);
+                            } else if (height >= mMaxHeight && height <= mBlurredViewHeight) {
+                                performAnimation(height, mMaxHeight);
+                            }
+                        } else {
+                            if (height >= 0 && height < mMinHeight) {
+                                performAnimation(height, mMinHeight);
+                            } else if (height >= mMinHeight && height < mDefaultHeight) {
+                                performAnimation(height, mMinHeight);
+                            } else if (height >= mDefaultHeight && height <= mMaxHeight) {
+                                performAnimation(height, mDefaultHeight);
+                                float alpha = ((float) (height - mDefaultHeight) / (float) (mMaxHeight - mDefaultHeight));
+                                changeAlpha(alpha, 0F);
+                            } else if (height >= mMaxHeight && height <= mBlurredViewHeight) {
+                                performAnimation(height, mMaxHeight);
                             }
                         }
                         break;
@@ -115,16 +144,19 @@ public class MainActivity extends AppCompatActivity {
 
     private void performAnimation(int start, int end) {
         ValueAnimator va = ValueAnimator.ofInt(start, end);
-        va.setDuration(300);
-        va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                int height = (int) animation.getAnimatedValue();
-                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) mPanelLayout.getLayoutParams();
-                params.height = height;
-                mPanelLayout.setLayoutParams(params);
-            }
+        va.setDuration(ANIMATION_DURATION);
+        va.addUpdateListener((animation) -> {
+            int height = (int) animation.getAnimatedValue();
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) mPanelLayout.getLayoutParams();
+            params.height = height;
+            mPanelLayout.setLayoutParams(params);
         });
         va.start();
+    }
+
+    private void changeAlpha(float start, float end) {
+        ObjectAnimator oa = ObjectAnimator.ofFloat(mDataThirdLine, "alpha", start, end);
+        oa.setDuration(ANIMATION_DURATION);
+        oa.start();
     }
 }
